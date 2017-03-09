98543463bf2621576184ee16b1c70aca0d9911a4
Adam Cooper <adam.cooper@nottingham.ac.uk>
2017-03-09 11:57:05 +0000

    Initial commit

---
+++ file.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+// IsDir checks to see if passed in path is a directory
+func IsDir(path string) (isdir bool) {
+	dir, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return dir.IsDir()
+}
+
+// IsFile checks to see if the passed in path is a file
+func IsFile(path string) (isFile bool) {
+	file, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return file.Mode().IsRegular()
+}
+
+// CopyFile copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file. The file mode will be copied from the source and
+// the copied data is synced/flushed to stable storage.
+func CopyFile(src string, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	err = os.Chown(dst,
+		int(si.Sys().(*syscall.Stat_t).Uid),
+		int(si.Sys().(*syscall.Stat_t).Gid))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// CopyDir recursively copies a directory tree, attempting to preserve permissions.
+// Source directory must exist, destination directory must *not* exist.
+// Symlinks are ignored and skipped.
+func CopyDir(src string, dst string) (err error) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !si.IsDir() {
+		return fmt.Errorf("source is not a directory")
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+
+	err = os.MkdirAll(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	err = os.Chown(dst,
+		int(si.Sys().(*syscall.Stat_t).Uid),
+		int(si.Sys().(*syscall.Stat_t).Gid))
+	if err != nil {
+		return
+	}
+
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			err = CopyDir(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		} else {
+			// Skip symlinks.
+			if entry.Mode()&os.ModeSymlink != 0 {
+				continue
+			}
+
+			err = CopyFile(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
+
+// LinkShared attempts to resolve the shared locations to actual files and
+// links them to the source locations.
+func LinkShared(shared []string, source string, destination string) (err error) {
+
+	for _, entry := range shared {
+		sourcePath := filepath.Join(source, entry)
+		destinationPath := filepath.Join(destination, entry)
+
+		logger.Infof("Linking %s as %s\n", entry, destinationPath)
+
+		absSourcePath, err := filepath.Abs(sourcePath)
+		if err != nil {
+			return err
+		}
+
+		err = os.RemoveAll(destinationPath)
+		if err != nil {
+			return err
+		}
+
+		if IsFile(absSourcePath) || IsDir(absSourcePath) {
+			os.Symlink(absSourcePath, destinationPath)
+		}
+	}
+
+	return
+}
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+
+	"os"
+	"path/filepath"
+
+	"github.com/juju/loggo"
+	"github.com/urfave/cli"
+	yaml "gopkg.in/yaml.v2"
+)
+
+var (
+	configFilename string
+	source         string
+	destination    string
+	debug          bool
+	logger         loggo.Logger
+)
+
+type conf struct {
+	Shared []string `yaml:"shared"`
+}
+
+func main() {
+	logger = loggo.GetLogger("")
+
+	app := cli.NewApp()
+	app.Name = "deploy"
+	app.Version = "1.0.0"
+	app.Usage = "Sets up modern PHP apps to work better when using docker"
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.Name}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} source destination
+
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+`
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "config, c",
+			Usage:       "Load configuration from `FILE`",
+			Value:       ".ddply",
+			Destination: &configFilename,
+			EnvVar:      "DEPLOY_CONFIG_FILE",
+		},
+		cli.BoolFlag{
+			Name:        "debug, d",
+			Usage:       "Increase verbosity of running messages",
+			Destination: &debug,
+		},
+	}
+
+	app.Action = func(c *cli.Context) (err error) {
+		if c.NArg() < 2 {
+			return cli.NewExitError("Source and/or destination not specified", 1)
+		}
+
+		source = c.Args().Get(0)
+		if !IsDir(source) {
+			return cli.NewExitError("Source argument does not point to valid directory", 2)
+		}
+
+		destination = c.Args().Get(1)
+
+		if c.IsSet("debug") {
+			logger.SetLogLevel(loggo.DEBUG)
+		}
+
+		var configPath = filepath.Join(source, configFilename)
+		if c.IsSet("config") {
+			configPath = configFilename
+		}
+
+		foundConfig := findConfig(configPath)
+		if c.IsSet("config") && !foundConfig {
+			return cli.NewExitError("Specified config file not found", 4)
+		}
+
+		yamlFile, err := ioutil.ReadFile(configPath)
+		if err != nil {
+			// no config file. assume link only
+			logger.Infof("No configuration file found or specified. Continuing with linked deploy")
+
+			var location = []string{""}
+			LinkShared(location, source, destination)
+		} else {
+			// config file. assume copy and link shared
+			var config conf
+			err := config.getConfig(yamlFile)
+			if err != nil {
+				return cli.NewExitError(err, 6)
+			}
+
+			// was able to find and read config file
+			logger.Debugf("Shared locations from config: %s\n", config.Shared)
+
+			CopyDir(source, destination)
+			LinkShared(config.Shared, source, destination)
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+func findConfig(path string) (foundConfig bool) {
+	file, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return file.Mode().IsRegular()
+}
+
+func (c *conf) getConfig(yamlFile []byte) (err error) {
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	return nil
+}
